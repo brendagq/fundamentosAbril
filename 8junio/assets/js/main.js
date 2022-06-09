a9f3a5eb3a90e98444b3b737f7812301326d0cb7
brendagq <brenda.gonzalez.dev@gmail.com>
2022-06-08 19:55:00 -0600

    avances clase

--- 8junio/assets/js/main.js
+++ 8junio/assets/js/main.js
@@ -1,3 +1,71 @@
 "use strict"
 
-/* MÉTODOS  DE ORDEN SUPERIOR*/
+/* 
+    MÉTODOS  DE ORDEN SUPERIOR
+*/
+
+const usuarios = [
+    {
+        id: 0,
+        nombre: "Fulanito Perez",
+        descripcion: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum sed id qui delectus possimus vitae.",
+        imgUrl: "https://picsum.photos/id/1005/200/300"
+    },
+    {
+        id: 1,
+        nombre: "Carlitos Sanchez",
+        descripcion: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum sed id qui delectus possimus vitae.",
+        imgUrl: "https://picsum.photos/id/1012/200/300"
+    },
+    {
+        id: 2,
+        nombre: "Perenganita Lopez",
+        descripcion: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum sed id qui delectus possimus vitae.",
+        imgUrl: "https://picsum.photos/id/1027/200/300"
+    },
+    {
+        id: 3,
+        nombre: "Maria Antonieta",
+        descripcion: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum sed id qui delectus possimus vitae.",
+        imgUrl: "https://picsum.photos/id/342/200/300"
+    },
+]
+/*
+function imprimir(elemento) {
+    console.log( elemento.nombre )
+}
+
+for (let i = 0; i < usuarios.length; i++) {
+    imprimir( usuarios[i] )    
+}
+*/
+
+/*
+let contenedor = document.getElementById("usuarios-contenedor")
+
+let fragment = new DocumentFragment()
+
+let retornoForEach = usuarios.forEach( (element) =>{
+    let h3 = document.createElement( "h3" )
+    h3.textContent = `${element.nombre}`
+    
+    fragment.appendChild( h3 )
+    return element
+})
+
+contenedor.appendChild( fragment )
+console.log( retornoForEach )
+*/
+let contenedor = document.getElementById("usuarios-contenedor")
+let fragment = new DocumentFragment()
+
+let retornoMap = usuarios.map( (element, index, array) => {
+    let h3 = document.createElement( "h3" )
+    h3.textContent = `${element.nombre}`
+    
+    fragment.appendChild( h3 )
+    return element.nombre
+})
+
+contenedor.appendChild( fragment )
+console.log( retornoMap )

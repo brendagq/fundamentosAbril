ddd9d170016ccdc601ff5ce5983ea50f5025219d
brendagq <brenda.gonzalez.dev@gmail.com>
2022-06-08 22:07:00 -0600

    clase 08junio

--- 8junio/assets/js/main.js
+++ 8junio/assets/js/main.js
@@ -88,3 +88,69 @@ let retornoFind = usuarios.find( (elemento) => {
 
 console.log( retornoFind )
 */
+
+/*
+Escribir una función que reciba como parametro un criterio de busqueda para los productos del ecommerce por tipo de prenda ("hoodies", "shirts", "sweatshirts") y muestra los resultados filtrados en la pantalla del navegador.
+Se debe visualizar el nombre, la imagen y el precio.
+Pueden utilizar el método innerHTML
+
+Hint: Para filtrar ocupa el metodo filter, para mostrar en la pantalla map o forEach.
+
+ */
+
+const items = [
+    {
+      id: 1,
+      name: 'Hoodies',
+      price: 14.00,
+      image: 'https://academlo-store.netlify.app/assets/img/featured1.png',
+      category: 'hoodies',
+      quantity: 10
+    },
+    {
+      id: 2,
+      name: 'Shirts',
+      price: 24.00,
+      image: 'https://academlo-store.netlify.app/assets/img/featured2.png',
+      category: 'shirts',
+      quantity: 15
+    },
+    {
+      id: 3,
+      name: 'Sweatshirts',
+      price: 24.00,
+      image: 'https://academlo-store.netlify.app/assets/img/featured3.png',
+      category: 'sweatshirts',
+      quantity: 20
+    },
+    {
+      id: 4,
+      name: 'Sweatshirts',
+      price: 30.00,
+      image: 'https://academlo-store.netlify.app/assets/img/featured3.png',
+      category: 'sweatshirts',
+      quantity: 10
+    }
+]
+
+
+function filtrarProductos( tipo ){
+    let contenedor = document.querySelector(".contenedor-productos")
+    let fragmento = ""
+
+    let resultadoProductos = items.filter( (item) => item.category === tipo )
+    
+    resultadoProductos.forEach( (producto) => {
+        fragmento += `
+        <div class="producto">
+            <img src=${producto.image} alt="">
+            <h3>${producto.name}</h3>
+            <small>${producto.price}</small>
+        </div>
+        `
+    })
+
+    contenedor.innerHTML = fragmento
+}
+
+filtrarProductos("hoodies")

4e63409832ce45acefc0df80c0013cc07d083ad9
brendagq <brenda.gonzalez.dev@gmail.com>
2022-06-09 22:09:07 -0600

    contador de dislikes

--- eventos/assets/js/main.js
+++ eventos/assets/js/main.js
@@ -21,10 +21,19 @@ let contadorLike = 0
 
 let likeBtn = document.querySelector("#btn-like")
 let small = document.querySelector("small")
+let dislikeBtn = document.querySelector("#btn-dislike")
 
 likeBtn.addEventListener( "click", (eventoData) =>{
     contadorLike++
     small.textContent = `A ${contadorLike} personas le gusta esta foto`
 })
 
-//Agregar el boton de dislike
+//Agregar el boton de dislike. Un dislike va a disminuir el numero de likes
+//No permitir que el numero de personas a las que les gusta la foto sea menor a 0 
+
+dislikeBtn.addEventListener( "click", (event) =>{
+    if( contadorLike > 0 ){
+        contadorLike -= 1
+        small.textContent = `A ${contadorLike} personas le gusta esta foto`
+    }
+})

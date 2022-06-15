64f14a688a6f69a5a67138c915996a78b0a5d302
brendagq <brenda.gonzalez.dev@gmail.com>
2022-06-14 19:35:33 -0600

    Pokemon input

--- 13junio/assets/js/main.js
+++ 13junio/assets/js/main.js
@@ -26,17 +26,30 @@ promesa
 
 const baseURL = "https://pokeapi.co/api/v2/"
 let contenedorPokemon = document.querySelector(".contenedor>section")
+let form = document.querySelector("form")
+let toast = document.querySelector(".toast")
 
-document.addEventListener( "DOMContentLoaded", () => fetchData() )
+document.addEventListener( "DOMContentLoaded", () => fetchData("pikachu") )
+form.addEventListener( "submit", (event) =>{
+    let input = form.querySelector("input")
+    let nombrePokemon = input.value.toLowerCase()
 
-function fetchData(){
-    fetch( `${baseURL}pokemon/mew` )
+    fetchData(nombrePokemon)
+
+    event.preventDefault()
+})
+
+function fetchData( nombrePokemon ){
+    fetch( `${baseURL}pokemon/${nombrePokemon}` )
     .then( respuesta => respuesta.json() ) //recibe la respuesta satisfactoria del servidor y la convierte a un formato adecuado
     .then( dataConvertida => {
         console.log( dataConvertida )
         mostrarPokemon( dataConvertida )
     }) //este segundo then() maneja la promesa que se genera en el primero por el metodo .json()
-    .catch( error => console.error(error))
+    .catch( error => {
+        console.error(error)
+        mostrarToast()
+    })
 }
 
 
@@ -47,7 +60,17 @@ function mostrarPokemon( data ){
     `
 }
 
+function mostrarToast(){
+    toast.classList.toggle("mostrar") //agregar la clase
+
+    setTimeout(() => {
+        toast.classList.toggle("mostrar") // quitarla
+    }, 5000);
+}
+
 
 //Modificar el codigo necesario para generar una busqueda dinamica de pokemones
 //Nombre del pokemon en la url del fetch SIEMPRE debe de ir en minusculas
 //Pikachu se va a mostrar por default al recargar la pagina
+
+

e939b3391f9739eaa4df9448609cfbf6e43f251c
brendagq <brenda.gonzalez.dev@gmail.com>
2022-06-14 20:02:17 -0600

    fetch post

---
+++ 14junio/assets/js/main.js
@@ -0,0 +1,43 @@
+"use strict"
+
+
+/*fetch( 'url', mensajeDeLaPeticion )
+
+GET --> Consulta de informacion
+POST --> Añadir nueva informacion
+PUT --> Actualizar informacion existente
+DELETE --> Elimina informacion existente
+PATCH --> Actualiza parcialmente
+
+*/
+/*
+fetch( 'urlPath', {
+    method: 'POST',
+    headers: {
+        'Content-Type' : 'application/json'
+    },
+    body: JSON.stringify({
+        name: "Brenda",
+        username: "bren123",
+        password: "123456"
+    })
+})
+
+*/
+
+fetch( "https://pokeapi.co/api/v2/", {
+    method: 'POST',
+    headers: {
+        'Content-Type' : 'application/json'
+    },
+    body: JSON.stringify({
+        name: "Brenda",
+        username: "bren123",
+        password: "123456",
+        urlImage: "imagenAlmacenadaEnOtroLado"
+    })
+})
+
+//Generar formulario de registro
+// nombre, username, password
+//Fetch fake de tipo POST con esa informacion
